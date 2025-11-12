a6e79e39b1e7efdee756a252ed276fd643a3eef9
AIGeniusAlien <amarra01@villanova.edu>
2025-11-11 19:27:02 -0500

    Refactor echo command in make_overleaf_zip.sh
    Updated echo command to be conditional and added newline.

--- shared/scripts/make_overleaf_zip.sh
+++ shared/scripts/make_overleaf_zip.sh
@@ -4,5 +4,3 @@ PAPER_DIR="${1:-paper1-system}"
 OUT="${PAPER_DIR}.overleaf.zip"
 zip -r "$OUT" "$PAPER_DIR" shared -x "*/.DS_Store" "*/build/*"
 echo "Wrote $OUT"
-
-chmod +x shared/scripts/make_overleaf_zip.sh
